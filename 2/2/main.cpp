1efc3539ae9f3e47175ac10cbe6296e022845ea3
kucherenkosveta <kucherenko55sss55@gmail.com>
2022-10-22 02:50:03 +0300

    5save

--- 2/2/main.cpp
+++ 2/2/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
   int a;
-  cout << "Введите число c клавиатуры6: ";
+  cout << "Введите число c клавиатуры: ";
   cin >> a;
   cin.ignore();
   cout << "Вы ввели: "<< a <<"\n";
